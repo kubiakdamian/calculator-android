71fa0d73827938df7cb521172ad35b22d483decd
unknown <202205@carbon.cti.p.lodz.pl>
2018-03-16 09:48:09 +0100

    Main page layout added

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.calculator;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        title = findViewById(R.id.title);
+        title.setTextColor(Color.WHITE);
     }
 }
